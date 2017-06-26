cfa3ecb82d0e9d3be7544cba219d88817e36ce3c
Julien Sarazin <julien.sarazin@me.com>
2017-06-26 19:21:38 +0200

    updated settings

--- settings/index.js
+++ settings/index.js
@@ -2,4 +2,6 @@ const _ = require('lodash');
 
 module.exports = settings => {
     _.merge(settings, require('./settings.json'));
+    if (process.env.PORT)
+      settings.port = process.env.PORT;
 }
